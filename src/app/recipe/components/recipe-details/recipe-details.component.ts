5576cd55ea0dcb9d28a14b1c5e3614b33f982bd7
Mo Afolayan <mo.afolayan@gmail.com>
2023-10-18 16:10:47 -0400

    moving add functionality out of recipe-details card

--- src/app/recipe/components/recipe-details/recipe-details.component.ts
+++ src/app/recipe/components/recipe-details/recipe-details.component.ts
@@ -10,42 +10,19 @@ import { IIngredient } from '../../ingredient';
 export class RecipeDetailsComponent implements OnInit {
 
     @Input() selectedRecipe: IRecipe = {} as IRecipe;
-    @Output() addIngredientEvent: EventEmitter<boolean> = new EventEmitter<boolean>();
     @Output() editIngredientEvent: EventEmitter<IIngredient> = new EventEmitter<IIngredient>();
-    @Output() deleteIngredientEvent: EventEmitter<IIngredient> = new EventEmitter<IIngredient>();
     @Output() deleteIngredientsEvent: EventEmitter<IIngredient[]> = new EventEmitter<IIngredient[]>();
-
-    // checkedIngredients: IIngredient[] = [];
 
     constructor() { }
 
     ngOnInit(): void {
     }
 
-    // updateCheckedIngredients(event: any, ingredient: IIngredient) {
-    //     if (event.checked) {
-    //         this.checkedIngredients.push(ingredient);
-    //     } else {
-    //         let index = this.checkedIngredients.findIndex(x => x.id == ingredient.id);
-    //         if (index != -1) {
-    //             this.checkedIngredients.splice(index, 1);
-    //         }
-    //     }
-    // }
-
-    addIngredient() {
-        this.addIngredientEvent.emit(true);
-    }
-
     editIngredient(ingredient: IIngredient) {
         this.editIngredientEvent.emit(ingredient);
     }
 
-    deleteIngredient(ingredient: IIngredient) {
-        this.deleteIngredientEvent.emit(ingredient);
-    }
-
-    deleteIngredients(ingredients: IIngredient[]) {
-        this.deleteIngredientsEvent.emit(ingredients);
+    deleteIngredients() {
+        this.deleteIngredientsEvent.emit();
     }
 }
